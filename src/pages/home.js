43b83aa0666c7af942656866fd7ad2177cae2c02
Greg Smith <gregalexsmith@gmail.com>
2020-10-16 20:58:45 -0400

    Add CI to github pages (#1)
    * deploy to github pages
    
    * add github url
    
    * update github build
    
    * ci-test
    
    * test-ci
    
    * test-ci
    
    * test-ci
    
    * add homepage
    
    * test ci

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import { Heading, Box } from '../components';
+import { Heading, Box, Link } from '../components';
 
 function Home() {
   return (
     <Box width={1}>
       <Heading>Home</Heading>
-      <p>A simple react starter application with a few preferences set up</p>
+      <p>A simple react starter application with a few preferences set up.</p>
+      <p>
+        <Link href="https://github.com/gregalexsmith/react-starter">
+          Github Repo
+        </Link>
+      </p>
     </Box>
   );
 }

d80a9010584ed075c211ea62e535184ca9f7dfa9
Greg Smith <gregalexsmith@gmail.com>
2020-10-16 20:55:33 -0400

    test ci

--- src/pages/home.js
+++ src/pages/home.js
@@ -5,7 +5,7 @@ function Home() {
   return (
     <Box width={1}>
       <Heading>Home</Heading>
-      <p>A simple react starter application with a few preferences set up</p>
+      <p>A simple react starter application with a few preferences set up.</p>
       <p>
         <Link href="https://github.com/gregalexsmith/react-starter">
           Github Repo

9438e2c6e00aebecdf14c184afb023c9a8f02dd6
greg smith <gregalexsmith@gmail.com>
2020-11-09 00:16:31 -0500

    fix log initalize

--- src/helpers/logger/analytics.js
+++ src/helpers/logger/analytics.js
@@ -1,11 +1,12 @@
 const CLIENT_ID = 'CLIENT_ID';
 
 const enabled = process.env.NODE_ENV === 'production';
+const present = !!window.analytics;
 
-const verify = () => enabled && window.analytics && window.analytics.page;
+const verify = () => enabled && present && window.analytics.page;
 
 const init = () => {
-  if (enabled) {
+  if (enabled && present) {
     window.analytics.load(CLIENT_ID);
   } else {
     console.log('analytics not loaded');
